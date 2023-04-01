c533a159e4eb4ec7e651d2a85b06b400493c6815
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-04-01 10:41:56 +0300

    Add kepler_data to planets model

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const app = require('./app');
+const { start } = require('repl');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
+}
+
+startServer();
