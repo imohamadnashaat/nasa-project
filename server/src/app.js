b6d25bf8929b49713ae3a2672fd9ebffba5fc66d
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-04-04 06:56:10 +0300

    Add logging requests with morgan package

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const cors = require('cors');
 const express = require('express');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -11,6 +12,7 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
