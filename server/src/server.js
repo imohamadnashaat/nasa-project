493e085bf1897365fe6636c43d8f296e03c28a39
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-04-12 15:34:36 +0300

    Add mongoose and connect to Mongo cluster

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
@@ -7,9 +8,21 @@ const { start } = require('repl');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  'mongodb+srv://nasa-api:kOcQVp6t57ey981t@nasacluster.bzswcbe.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(error);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
