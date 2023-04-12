c548d96a3db45158bdd48d68e96134579e6a7826
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-04-12 15:36:49 +0300

    Add launches mongoose schema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: {
+    type: [String],
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+});
+
+// Connects launchesSchema with "launches" collection
+module.exports = mongoose.model('Launch', launchesSchema);
