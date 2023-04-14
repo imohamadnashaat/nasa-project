c6b36f3504221a321a48edfa3049643f22b37ccf
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-04-14 15:01:13 +0300

    Move mongoose connection to mongo file

---
+++ server/src/services/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL =
+  'mongodb+srv://nasa-api:kOcQVp6t57ey981t@nasacluster.bzswcbe.mongodb.net/nasa?retryWrites=true&w=majority';
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(error);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
