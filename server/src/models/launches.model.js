d139da18eb87b08d0bab65a5b2e71e8e75ce00b6
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-04-16 14:32:44 +0300

    Add launches pagination

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: 'Kepler Exploration X', //name
-  rocket: 'Explorer IS1', //rocket.name
-  launchDate: new Date('April 25, 2030'), //date_local
-  target: 'Kepler-442 b', //not applicable
-  customers: ['Nashaat', 'NASA'], //payloads.customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
 async function populateLaunches() {
@@ -65,8 +52,6 @@ async function populateLaunches() {
       customers,
     };
 
-    console.log(`${launch.flightNumber} ${launch.mission}`);
-
     saveLaunch(launch);
   }
 }
@@ -105,8 +90,12 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launches.find({}, { _id: 0, __v: 0 });
+async function getAllLaunches(skip, limit) {
+  return await launches
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
